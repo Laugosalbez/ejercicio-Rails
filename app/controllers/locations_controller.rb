8eadc910d7c37eabcef6dae179eafa605577c2de
Laura <lau.goga@gmail.com>
2014-11-04 13:44:42 +0100

    nueva

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -12,11 +12,3 @@ class LocationsController < ApplicationController
 	
 end
 
-class ApplicationController < ActionController :: Base
-	protect_from_forgery with: :exception
-
-	rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
-private
-	def record_not_found
-		render status: 404, file: "#{Rails.root}/public/404"
-	end
